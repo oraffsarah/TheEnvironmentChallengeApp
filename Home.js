1f0f72f74e56bfd789925e3ae95bc71ceb237339
DeclanDavis <146258203+DeclanDavis@users.noreply.github.com>
2024-02-07 16:10:51 +0000

    Create Home.js

---
+++ Home.js
@@ -0,0 +1,53 @@
+export default function Home() {
+  return (
+    <>
+      <h2 class="home-container">
+        {" "}
+        Welcome to Project Everest's Environmental Challenge!{" "}
+      </h2>
+      <div className="home-content-container">
+        <div className="home-content">
+          <div>
+            <h4>
+              Your personal journey to make a positive impact on the environment
+              begins now 🌍
+            </h4>
+            <hr></hr>
+          </div>
+          <div>
+            🌱 Do you want to make a difference in the world while earning
+            rewards? Look no further! The Environment Challenge is your go-to
+            app for fun and meaningful challenges that contribute to a greener
+            planet.
+          </div>
+          <div>
+            {" "}
+            🌿 Every week, we present you with challenges designed to reduce
+            plastic usage, minimize carbon emissions, and foster eco-friendly
+            habits. From saying no to single-use plastics to opting for
+            sustainable transportation, each challenge you conquer earns you
+            points.{" "}
+          </div>
+          <div>
+            {" "}
+            📊 Track your progress and witness the positive change you're
+            making, compare your weekly scores, and explore achievements through
+            our Statistics page.{" "}
+          </div>
+          <div>
+            {" "}
+            💚 Join the community of eco-warriors dedicated to creating a
+            sustainable future. Every action counts, and with The Environment
+            Challenge, you're not just earning points - you're contributing to a
+            healthier, happier planet.{" "}
+          </div>
+          <div>
+            {" "}
+            Ready to embark on your environmental journey? Let's make a lasting
+            impact, one challenge at a time! 🌟{" "}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
