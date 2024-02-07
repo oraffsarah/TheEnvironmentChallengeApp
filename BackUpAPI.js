05fe1644f33989518a60e56c4519a6e395b56ed6
DeclanDavis <146258203+DeclanDavis@users.noreply.github.com>
2024-02-07 16:16:30 +0000

    Create BackUpAPI.js

---
+++ BackUpAPI.js
@@ -0,0 +1,98 @@
+{ "challenges" : [ 
+      { 
+        "id" : 1,
+        "title" : "Bring Your Own Reusable Cup",
+        "level" : "Easy",
+        "description" : "Opt For A Reusable Cup For Your Hot Beverages Instead Of Relying On Disposable, Single-Use Cups.",
+        "points" : { 
+            "awarded" : 5,
+            "threshold" : 0
+          },
+        "status" : "",
+        "tip" : "Congratulations on completing the challenge! By choosing a reusable cup for your hot drinks, you've made a positive impact on the environment. Every small action counts, and your choice contributes to reducing waste and promoting sustainability. Thank you for being mindful and making a difference! "
+      },
+      { 
+        "id" : 2,
+        "title" : "Bring Your Own Reusable Grocery Bag ",
+            "level" : "Easy",
+        "description" : "Opt To Take Your Own Eco-Friendly Bag When Buying Or Carrying Your Groceries",
+        "points" : { 
+            "awarded" : 5,
+            "threshold" : 0
+          },
+        "status" : "",
+        "tip" : "Well done on completing the challenge! By choosing an eco-friendly bag for your goods and groceries, you're actively supporting a sustainable lifestyle. Your decision to reduce single-use plastic and embrace environmentally friendly alternatives makes a meaningful impact. Thank you for making a positive difference for our planet!"
+      },
+      { 
+        "id" : 3,
+        "title" : "Choose Public Transport Instead Of Using Your Own Vehicle",
+            "level" : "Easy",
+        "description" : "Consider Using Public Transportation Such As Buses Or Trains Instead Of Driving Your Own Personal Vehicle To Reduce Emissions And Contribute To A Greener Environment.",
+        "points" : { 
+            "awarded" : 20,
+            "threshold" : 0
+          },
+        "status" : "",
+        "tip" : "Congratulations On Completing The Challenge! Opting For Public Transportation Instead Of Driving Not Only Makes Your Commute More Sustainable But Also Helps Reduce Emissions, Easing The Impact On The Environment. Your Choice Contributes To A Cleaner, Healthier Planet. Thank You for Taking Steps Toward A More Sustainable And Eco-friendly lifestyleifestyle!"
+      },
+      { 
+        "id" : 4,
+        "title" : "Take A Shower Instead Of A Bath",
+            "level" : "Medium",
+        "description" : "Try Taking A Shower Instead Of Running A Bath To Help Reduce Unnessecary Water Usage",
+        "points" : { 
+            "awarded" : 25,
+            "threshold" : 25
+          },
+        "status" : "",
+        "tip" : "Great job on completing the challenge! Choosing to take a shower instead of running a bath is a fantastic way to conserve water and contribute to water efficiency. Your mindful decision reflects a commitment to sustainability. Thank you for being a water-conscious individual and making a positive impact on our environment!"
+      },
+      { 
+        "id" : 5,
+        "title" : "Keep Your Shower Time Under 5 Minutes",
+            "level" : "Hard",
+        "description" : "Challenge Yourself To Limit Your Shower To Under 5 Minutes",
+        "points" : { 
+            "awarded" : 50,
+            "threshold" : 50
+          },
+        "status" : "",
+        "tip" : "Well done on completing the challenge. By reducing the time that you are in the shower you are improving your personal water efficency, aswell as reducing the energy that you are using while taking one!"
+      },
+      { 
+        "id" : 6,
+        "title" : "Try A Vegetarian Diet For The Week",
+            "level" : "Medium",
+        "description" : "Avoid Eating Meat For 7 Days",
+        "points" : { 
+            "awarded" : 40,
+            "threshold" : 25
+          },
+        "status" : "",
+        "tip" : "Congratulations on completing the challenge to avoid eating meat for 7 days! Your commitment to a plant-based diet has positive effects on the environment. By reducing meat consumption, you've contributed to lowering your carbon footprint and promoting sustainable food choices.Thank you for making a meaningful impact and taking steps toward a more eco-conscious lifestyle!"
+      },
+      { 
+        "id" : 7,
+        "title" : "Avoid purchasing Items That Use Unsustainable Materials",
+            "level" : "Hard",
+        "description" : "Opt To Purchase Goods That Are Recycable And Are Produced With Sustainable Materials",
+        "points" : { 
+            "awarded" : 60,
+            "threshold" : 50
+          },
+        "status" : "",
+        "tip" : "Fantastic job on completing the challenge to opt for goods that are recyclable and produced with sustainable materials! Your conscious consumer choices play a vital role in promoting a greener and more eco-friendly world. By supporting products with a focus on recyclability and sustainability, you're contributing to a positive environmental impact. Thank you for making thoughtful decisions that prioritize the well-being of our planet. Keep up the great work!"
+      },
+      { 
+        "id" : 8,
+        "title" : "Zero Waste",
+            "level" : "Hard",
+        "description" : " Produce Zero Non-Compostable Or Non-Recyclable Waste For The Day",
+        "points" : { 
+            "awarded" : 75,
+            "threshold" : 50
+          },
+        "status" : "",
+        "tip" : "Congratulations on successfully producing zero non-compostable or non-recyclable waste for the day! Your dedication to minimizing environmental impact is truly commendable. By prioritizing compostable and recyclable options, you've significantly reduced your ecological footprint. Thank you for being a responsible steward of the planet and making a positive difference. Keep up the fantastic work!"
+      }
+    ] }
